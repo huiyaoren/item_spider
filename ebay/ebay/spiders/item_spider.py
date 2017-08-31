c05047dfc6bc337c489cb3e234ec03827cc59ab6
hanlin <hanlin>
2017-08-31 17:40:13 +0800

    newFile | item_spider.py | 程序主体

---
+++ ebay/ebay/spiders/item_spider.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+from scrapy import Item, Field, Request, Spider
+
+
+class ItemSpider(Spider):
+    name = "item_spider"
+    start_urls = [
+        'http://www.baidu.com',
+        'http://www.baidu.com',
+        'http://www.baidu.com',
+    ]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield Request(url, dont_filter=True)
+
+    def parse(self, response):
+        pass
+
+
+class LinkItem(Item):
+    name = Field()
+    link = Field()
