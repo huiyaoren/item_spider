3a611d620726b1f6946b62fc9a417ddea8a19a81
hanlin <hanlin>
2017-09-02 13:34:09 +0800

    newFeature | 数据库连接

--- ebay/ebay/utils/common.py
+++ ebay/ebay/utils/common.py
@@ -2,17 +2,35 @@
 from pymongo import MongoClient
 from pymysql import connect
 
+from ..configs.database_config import Config
 
-def db_mongodb(db_name='test_database', uri='mongodb://localhost:27017/'):
+
+def db_mongodb_base(db_name, uri):
     Client = MongoClient(uri)
     db = Client[db_name]
     return db
 
 
-def db_mysql(db_name='erp', host='192.168.1.248', username='root', password='root'):
+def db_mongodb():
+    return db_mongodb_base(
+        Config['mongodb']['database'],
+        'mongodb://{0}:{1}/'.format(Config['mongodb']['host'], Config['mongodb']['port'])
+    )
+
+
+def db_mysql_base(db_name, host, username, password):
     db = connect(host, username, password, db_name)
     return db
 
 
+def db_mysql():
+    return db_mysql_base(
+        Config['mysql']['database'],
+        Config['mysql']['host'],
+        Config['mysql']['username'],
+        Config['mysql']['password'],
+    )
+
+
 if __name__ == '__main__':
-    pass
+    db_mysql()
