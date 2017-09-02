651aa00e7ba69f193605a1d5943cb68b143d6518
hanlin <hanlin>
2017-09-02 14:59:22 +0800

    fixBug | Pipeline 数据库写入

--- ebay/ebay/pipelines.py
+++ ebay/ebay/pipelines.py
@@ -1,14 +1,29 @@
 # -*- coding: utf-8 -*-
-
+import logging
 from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
 
+from .utils.common import db_mysql, db_mongodb
+
+
+logger = logging.getLogger(__name__)
 
 class EbayPipeline(object):
     def __init__(self):
-        # MongoDB 数据库连接 todo-1
-        Client = MongoClient()
-        self.db = Client['test_database']
+
+        self.db = db_mongodb()
         self.collection = self.db['test_collection']
+        self.collection.ensure_index('itemId', unique=True)
 
     def process_item(self, item, spider):
-        return item
+
+        listing = item['data']
+        logger.info(listing)
+        c = self.collection
+
+        try:
+            c.insert_one(listing)
+        except DuplicateKeyError:
+            logger.info("Duplicate Item")
+        else:
+            return
