5692d24584f985f7eaec6a6bd2e6b90be3c0e8ab
hanlin <hanlin>
2017-09-02 14:39:52 +0800

    newFeature | 商品分类数据转移

--- ebay/run.py
+++ ebay/run.py
@@ -10,5 +10,41 @@ def run():
     process.start()
 
 
+def get_category_ids():
+    from ebay.utils.common import db_mysql
+    mysql = db_mysql()
+    cursor = mysql.cursor()
+    sql = 'select platform_category_id from erp_saas_goods_category where site = 201'
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            print(row[0])
+            data = {'category_id': row[0]}
+            yield data
+    except:
+        print("Error: unable to fetch data")
+    else:
+        mysql.close()
+    return
+
+
+def insert_category_ids():
+    import logging
+    from pymongo.errors import DuplicateKeyError
+    from ebay.utils.common import db_mongodb
+    mongodb = db_mongodb()
+    c = mongodb['category_ids']
+    c.ensure_index('category_id', unique=True)
+    for listing in get_category_ids():
+        try:
+            c.insert_one(listing)
+        except DuplicateKeyError:
+            print("Duplicate ")
+            logging.info("Duplicate Item")
+    print('count: ', c.count())
+
+
 if __name__ == '__main__':
-    run()
+    # run()
+    insert_category_ids()
