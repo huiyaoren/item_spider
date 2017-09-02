316bb7dba35dc69314f677800b6fe576976c5f65
hanlin <hanlin>
2017-09-02 14:57:47 +0800

    newFeature | 结构调整

--- ebay/ebay/utils/common.py
+++ ebay/ebay/utils/common.py
@@ -2,7 +2,9 @@
 from pymongo import MongoClient
 from pymysql import connect
 
+import logging
 from ..configs.database_config import Config
+from pymongo.errors import DuplicateKeyError
 
 
 def db_mongodb_base(db_name, uri):
@@ -16,6 +18,13 @@ def db_mongodb():
         Config['mongodb']['database'],
         'mongodb://{0}:{1}/'.format(Config['mongodb']['host'], Config['mongodb']['port'])
     )
+
+
+def category_ids():
+    db = db_mongodb()
+    c = db.category_ids
+    for data in c.find()[:100]:
+        yield data['category_id']
 
 
 def db_mysql_base(db_name, host, username, password):
@@ -32,5 +41,36 @@ def db_mysql():
     )
 
 
+def get_category_ids():
+    mysql = db_mysql()
+    cursor = mysql.cursor()
+    sql = 'select platform_category_id from erp_saas_goods_category where site = 201'
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            print(row[0])
+            data = {'category_id': row[0]}
+            yield data
+    except:
+        print("Error: unable to fetch data")
+    else:
+        mysql.close()
+    return
+
+
+def insert_category_ids():
+    mongodb = db_mongodb()
+    c = mongodb['category_ids']
+    c.ensure_index('category_id', unique=True)
+    for listing in get_category_ids():
+        try:
+            c.insert_one(listing)
+        except DuplicateKeyError:
+            print("Duplicate ")
+            logging.info("Duplicate Item")
+    print('count: ', c.count())
+
+
 if __name__ == '__main__':
-    db_mysql()
+    pass
--- ebay/run.py
+++ ebay/run.py
@@ -10,41 +10,11 @@ def run():
     process.start()
 
 
-def get_category_ids():
-    from ebay.utils.common import db_mysql
-    mysql = db_mysql()
-    cursor = mysql.cursor()
-    sql = 'select platform_category_id from erp_saas_goods_category where site = 201'
-    try:
-        cursor.execute(sql)
-        results = cursor.fetchall()
-        for row in results:
-            print(row[0])
-            data = {'category_id': row[0]}
-            yield data
-    except:
-        print("Error: unable to fetch data")
-    else:
-        mysql.close()
-    return
-
-
-def insert_category_ids():
-    import logging
-    from pymongo.errors import DuplicateKeyError
-    from ebay.utils.common import db_mongodb
-    mongodb = db_mongodb()
-    c = mongodb['category_ids']
-    c.ensure_index('category_id', unique=True)
-    for listing in get_category_ids():
-        try:
-            c.insert_one(listing)
-        except DuplicateKeyError:
-            print("Duplicate ")
-            logging.info("Duplicate Item")
-    print('count: ', c.count())
+def test():
+    from ebay.utils import common
+    common.insert_category_ids()
 
 
 if __name__ == '__main__':
     # run()
-    insert_category_ids()
+    test()
