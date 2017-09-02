ad23c1f4f2b1fb15ab697f52d5d03daf333d6e48
hanlin <hanlin>
2017-09-02 13:34:32 +0800

    newFile | 数据库配置

---
+++ ebay/ebay/configs/database_config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+Config = {
+    'mysql': {
+        'host': '192.168.1.248',
+        'database': 'erp',
+        'username': 'root',
+        'password': 'root',
+        'port': '3306',
+    },
+    'mongodb': {
+        'host': 'localhost',
+        'database': 'test_database',
+        'port': '27017'
+    }
+}
