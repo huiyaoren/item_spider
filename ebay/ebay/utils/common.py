bf95f3dc016973053e37ebd176bec90d66f10ab9
hanlin <hanlin>
2017-09-04 17:47:08 +0800

    newFeature | 清洗 item_id | 日期相关函数

--- ebay/ebay/utils/common.py
+++ ebay/ebay/utils/common.py
@@ -1,78 +1,29 @@
 # -*- coding: utf-8 -*-
-from pymongo import MongoClient
-from pymysql import connect
-
-import logging
-from ..configs.database_config import Config
-from pymongo.errors import DuplicateKeyError
-
-logger = logging.getLogger(__name__)
+from datetime import datetime
 
 
-def db_mongodb_base(db_name, uri):
-    Client = MongoClient(uri)
-    db = Client[db_name]
-    return db
+def date():
+    return datetime.now().strftime("%Y%m%d")
 
 
-def db_mongodb():
-    return db_mongodb_base(
-        Config['mongodb']['database'],
-        'mongodb://{0}:{1}/'.format(Config['mongodb']['host'], Config['mongodb']['port'])
-    )
+def next_date(date):
+    # todo 后一天日期
+    pass
 
 
-def category_ids():
-    db = db_mongodb()
-    c = db.category_ids
-    # todo-1 改为 pop 操作 | 取一条删一条
-    for data in c.find():
-        yield data['category_id']
+def previous_date(date):
+    # todo 前一天日期
+    pass
 
 
-def db_mysql_base(db_name, host, username, password):
-    db = connect(host, username, password, db_name)
-    return db
-
-
-def db_mysql():
-    return db_mysql_base(
-        Config['mysql']['database'],
-        Config['mysql']['host'],
-        Config['mysql']['username'],
-        Config['mysql']['password'],
-    )
-
-
-def category_ids_from_mysql():
-    mysql = db_mysql()
-    cursor = mysql.cursor()
-    sql = 'select platform_category_id from erp_saas_goods_category where site = 201'
-    try:
-        cursor.execute(sql)
-        results = cursor.fetchall()
-        for row in results:
-            print(row[0])
-            data = {'category_id': row[0]}
-            yield data
-    except:
-        print("Error: unable to fetch data")
-        logger.info("Mysql error")
-    else:
-        mysql.close()
-
-
-def insert_category_ids():
-    mongodb = db_mongodb()
-    c = mongodb['category_ids']
-    c.ensure_index('category_id', unique=True)
-    for listing in category_ids_from_mysql():
-        try:
-            c.insert_one(listing)
-        except DuplicateKeyError:
-            print("Duplicate ")
-    print('count: ', c.count())
+def clean_item_id(item_id='v1|292191373015|591122882367'):
+    '''
+    :param item_id: v1|292191373015|591122882367
+    :return: 292191373015
+    '''
+    i = item_id.split('|')[1]
+    return i
 
 
 if __name__ == '__main__':
-    pass
+    date()
