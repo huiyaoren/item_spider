397f5b9010f390ccc5ae170fe80d788e48e0967e
hanlin <hanlin>
2017-09-06 18:42:47 +0800

    newConfig | redis

--- ebay/ebay/configs/database_config.py
+++ ebay/ebay/configs/database_config.py
@@ -8,9 +8,18 @@ Config = {
         'password': 'root',
         'port': '3306',
     },
+    'mongodb_remote': {
+        'host': '192.168.1.200',
+        'database': 'item_data',
+        'port': 27017,
+    },
     'mongodb': {
         'host': 'localhost',
         'database': 'test_database',
-        'port': '27017'
+        'port': 27017,
+    },
+    'redis': {
+        'host': 'localhost',
+        'password': 123,
     }
 }
