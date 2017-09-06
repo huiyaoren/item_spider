c50d06976c11cc89ae9bef4348cd9d53a36eb542
hanlin <hanlin>
2017-09-06 18:38:10 +0800

    newFeature | insert_category_ids_to_redis

--- ebay/ebay/utils/data.py
+++ ebay/ebay/utils/data.py
@@ -9,8 +9,8 @@ from ..configs.database_config import Config
 logger = logging.getLogger(__name__)
 
 
-def db_mongodb_base(db_name, uri):
-    Client = MongoClient(uri)
+def db_mongodb_base(db_name, host, port):
+    Client = MongoClient(host, port)
     db = Client[db_name]
     return db
 
@@ -18,8 +18,8 @@ def db_mongodb_base(db_name, uri):
 def db_mongodb():
     return db_mongodb_base(
         Config['mongodb']['database'],
-        'mongodb://{0}:{1}/'.format(Config['mongodb']['host'], Config['mongodb']['port'])
-    )
+        Config['mongodb']['host'],
+        Config['mongodb']['port'])
 
 
 def category_ids():
@@ -45,7 +45,7 @@ def db_mysql():
 
 
 def db_redis():
-    redis = Redis(password=Config['redis']['password'])
+    redis = Redis()
     return redis
 
 
@@ -67,9 +67,19 @@ def category_ids_from_mysql():
         mysql.close()
 
 
-def insert_category_ids():
-    mongodb = db_mongodb()
-    c = mongodb['category_ids']
+def insert_category_ids(to='mongodb', db=None):
+    if to == 'mongodb':
+        insert_category_ids_to_mongodb(db)
+    elif to == 'redis':
+        insert_category_ids_to_redis(db)
+
+
+def insert_category_ids_to_mongodb(mongodb=None):
+    ''' category_ids 从 mysql 转移至 mongodb '''
+    m = mongodb
+    if m is None:
+        m = db_mongodb()
+    c = m['category_ids']
     c.ensure_index('category_id', unique=True)
     for listing in category_ids_from_mysql():
         try:
@@ -79,14 +89,45 @@ def insert_category_ids():
     print('count: ', c.count())
 
 
-def token_from_redis():
-    # 从 redis 获取 token
+def insert_category_ids_to_redis(redis=None):
+    ''' category_ids 从 mysql 转移至 redis '''
+    r = redis
+    if r is None:
+        r = db_redis()
+    for listing in category_ids_from_mysql():
+        try:
+            r.lpush('ebay:category_ids', listing.get('category_id'))
+        except:
+            print("Redis Error")
+    print('Done')
+
+
+def token_from_redis(redis):
+    # 从 redis 获取 token todo
     pass
 
 
-def category_id_from_redis():
-    # 从 redis 获取 category_id
+def keep_token_available():
+    # todo 定期获取 token
     pass
+
+
+def category_id_from_redis(redis):
+    # 从 redis 获取 category_id todo
+    pass
+
+
+def item_id_from_redis(redis):
+    # 从 redis 获取 category_id todo
+    pass
+
+
+def item_id_is_duplicated(item_id, redis):
+    ''' 商品分类是否重复 todo
+    :param category_id: 
+    :return: 
+    '''
+    return False
 
 
 if __name__ == '__main__':
