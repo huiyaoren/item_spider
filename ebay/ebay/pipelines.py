f8b87ee03a7b821b18efc8fb695f95df148a7fb7
hanlin <hanlin>
2017-09-08 16:00:58 +0800

    newFeature | 增加 item 处理方法

--- ebay/ebay/pipelines.py
+++ ebay/ebay/pipelines.py
@@ -15,13 +15,30 @@ class EbayPipeline(object):
         self.date = date()
         self.collection = self.mongodb['c_{0}'.format(self.date)]
         self.collection.ensure_index('itemId', unique=True)
+        self.collection_detail = self.mongodb['d_{0}'.format(self.date)]
 
         # self.mysql = db_mysql()
         # self.cursor = self.mysql.cursor()
 
     def process_item(self, item, spider):
-        c = self.collection
+        if spider.name == 'listing_redis_spider':
+            self.process_item_listing(item, spider)
+        elif spider.name == 'detail_xml_redis_spider':
+            self.process_item_detail(item, spider)
 
+    def process_item_detail(self, item, spider):
+        d = self.collection_detail
+        item['date'] = self.date
+        logger.info(item)
+        try:
+            d.insert_one(item)
+        except:
+            logger.info("Mongodb Error")
+        else:
+            return item
+
+    def process_item_listing(self, item, spider):
+        c = self.collection
         listing = {}
         listing['price'] = item.get('price')
         listing['title'] = item.get('title')
@@ -29,13 +46,11 @@ class EbayPipeline(object):
         listing['itemLocation'] = item.get('itemLocation')
         listing['categories'] = item.get('categories')
         listing['image'] = item.get('image')
-        listing['itemId'] = clean_item_id(item.get('itemId'))
+        listing['itemId'] = item.get('itemId')
         listing['itemWebUrl'] = item.get('itemWebUrl')
         listing['date'] = self.date
-
         logger.info(listing)
         # return listing
-
         try:
             c.insert_one(listing)
         except DuplicateKeyError:
