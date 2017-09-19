b0657336338a7786fe8f998caad18c07fb05e40d
hanlin <hanlin>
2017-09-19 18:00:53 +0800

    fixFeature | transfer

--- ebay/transfer.py
+++ ebay/transfer.py
@@ -1,8 +1,8 @@
 from ebay.utils.data import insert_items_into_mysql
 
 
-def run():
-    insert_items_into_mysql()
+def run(day=None):
+    insert_items_into_mysql(day)
 
 
 if __name__ == '__main__':
