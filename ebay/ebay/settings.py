5ca94d52ab4b7bfd42d6f24a6141a83f1b3f8d55
hanlin <hanlin>
2017-10-10 10:15:32 +0800

    fixFeature | setting

--- ebay/ebay/settings.py
+++ ebay/ebay/settings.py
@@ -32,12 +32,12 @@ LOG_FILE = None
 LOG_LEVEL = 'DEBUG' # fixme INFO
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 258
+CONCURRENT_REQUESTS = 256
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3 # fixme 0
+DOWNLOAD_DELAY = 1 # fixme 0
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
