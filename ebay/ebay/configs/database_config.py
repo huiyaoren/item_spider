b4e7d8fd3f3ad32e06ff0ff3e7af2ac61084c9dc
hanlin <hanlin>
2017-10-18 15:56:31 +0800

    fixFeature | mysql_remote

--- ebay/ebay/configs/database_config.py
+++ ebay/ebay/configs/database_config.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 config = {
-    # 'mysql': {
-    #     'host': '192.168.1.248',
-    #     'database': 'erp_spider',
-    #     'username': 'root',
-    #     'password': 'root',
-    #     'port': 3306,
-    # },
     'mysql': {
+        'host': '192.168.1.248',
+        'database': 'erp_spider',
+        'username': 'root',
+        'password': 'root',
+        'port': 3306,
+    },
+    'mysql_remote': {
         'host': '45.126.121.187',
         'database': 'erp_spider',
         'username': 'erp_spider',
