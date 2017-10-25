2449087827c6b6cadcaac402fc4d24248ffb1c23
hanlin <hanlin>
2017-10-25 14:19:35 +0800

    fixFeature | parse

--- ebay/ebay/spiders/listing_xml_redis_spider.py
+++ ebay/ebay/spiders/listing_xml_redis_spider.py
@@ -50,11 +50,12 @@ class ListingXmlRedisSpider(RedisSpider):
         t = etree.HTML(bytes(response.text, encoding='utf8'))
         s = '/html/body/finditemsbycategoryresponse'
 
+        # 异常处理
         ack = t.xpath(s + '/ack/text()')
         for ack_str in ack:
             if ack_str != 'Success':
-                print(response.text)
-
+                logger.warning('Response fail or warning. response: \n{0}'.format(response.text))
+            break
 
         # 商品分类
         for page_current in t.xpath(s + '/paginationoutput/pagenumber/text()'):
@@ -64,6 +65,8 @@ class ListingXmlRedisSpider(RedisSpider):
 
                 page_total = t.xpath(s + '/paginationoutput/totalpages/text()')[0]
                 for page in range(2, int(page_total) + 1):
+                    if page > 100:
+                        break
                     self.server.lpush('ebay:category_ids', '{0}:{1}'.format(category_id, page))
             break
 
