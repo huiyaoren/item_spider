4e2f748fd84047e445654737c701e32bc5e7738e
hanlin <hanlin>
2017-10-26 16:24:20 +0800

    fixFeature | DOWNLOAD_TIMEOUT = 360

--- ebay/ebay/settings.py
+++ ebay/ebay/settings.py
@@ -39,6 +39,7 @@ CONCURRENT_ITEMS = 1024
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 DOWNLOAD_DELAY = 1 # fixme 0
+DOWNLOAD_TIMEOUT = 360
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
