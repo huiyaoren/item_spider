688b2851533c967ee0434ef1c97ed3e260a0da50
hanlin <hanlin>
2017-10-30 15:57:17 +0800

    newConfig | DATABASE_CONFIG

--- ebay/ebay/settings.py
+++ ebay/ebay/settings.py
@@ -106,3 +106,35 @@ ITEM_PIPELINES = {
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
 REDIS_URL = 'redis://192.168.1.192:6379'
+
+DATABASE_CONFIG = {
+    'mysql_local': {
+        'host': '192.168.1.248',
+        'database': 'erp_spider',
+        'username': 'root',
+        'password': 'root',
+        'port': 3306,
+    },
+    'mysql': {
+        'host': '45.126.121.187',
+        'database': 'erp_spider',
+        'username': 'erp_spider',
+        'password': 'fyEnzfwZjT',
+        'port': 3306,
+    },
+    'mongodb_remote': {
+        'host': '192.168.1.192',
+        'database': 'test_database',
+        'port': 27017,
+    },
+    'mongodb': {
+        'host': 'localhost',
+        'database': 'test_database',
+        'port': 27017,
+    },
+    'redis': {
+        'host': '192.168.1.192',
+        'password': 'root',
+        'port': 6379,
+    }
+}
