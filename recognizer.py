ba0c49f7c631075fec100451825e90fcea8ccbc1
hanlin <hanlin>
2017-11-03 17:52:11 +0800

    newFile | recognizer.py | 打码平台接口连接

---
+++ recognizer.py
@@ -0,0 +1,123 @@
+import http.client, mimetypes, urllib, json, time, requests
+
+
+class Connection:
+    apiurl = 'http://api.yundama.com/api.php'
+    username = ''
+    password = ''
+    appid = ''
+    appkey = ''
+
+    def __init__(self, username, password, appid, appkey):
+        self.username = username
+        self.password = password
+        self.appid = str(appid)
+        self.appkey = appkey
+
+    def request(self, fields, files=None):
+        files = files or {}
+        response = self.post_url(self.apiurl, fields, files)
+        response = json.loads(response)
+        return response
+
+    def balance(self):
+        data = {'method': 'balance', 'username': self.username, 'password': self.password, 'appid': self.appid,
+                'appkey': self.appkey}
+        response = self.request(data)
+        if response:
+            if (response['ret'] and response['ret'] < 0):
+                return response['ret']
+            else:
+                return response['balance']
+        else:
+            return -9001
+
+    def login(self):
+        data = {'method': 'login', 'username': self.username, 'password': self.password, 'appid': self.appid,
+                'appkey': self.appkey}
+        response = self.request(data)
+        if response:
+            if (response['ret'] and response['ret'] < 0):
+                return response['ret']
+            else:
+                return response['uid']
+        else:
+            return -9001
+
+    def upload(self, filename, codetype, timeout):
+        data = {'method': 'upload', 'username': self.username, 'password': self.password, 'appid': self.appid,
+                'appkey': self.appkey, 'codetype': str(codetype), 'timeout': str(timeout)}
+        file = {'file': filename}
+        response = self.request(data, file)
+        if (response):
+            if (response['ret'] and response['ret'] < 0):
+                return response['ret']
+            else:
+                return response['cid']
+        else:
+            return -9001
+
+    def result(self, cid):
+        data = {'method': 'result', 'username': self.username, 'password': self.password, 'appid': self.appid,
+                'appkey': self.appkey, 'cid': str(cid)}
+        response = self.request(data)
+        return response and response['text'] or ''
+
+    def decode(self, filename, codetype=4006, timeout=60):
+        cid = self.upload(filename, codetype, timeout)
+        if (cid > 0):
+            for i in range(0, timeout):
+                result = self.result(cid)
+                if (result != ''):
+                    return cid, result
+                else:
+                    time.sleep(1)
+            return -3003, ''
+        else:
+            return cid, ''
+
+    def post_url(self, url, fields, files=None):
+        files = files or {}
+        for key in files:
+            files[key] = open(files[key], 'rb')
+        res = requests.post(url, files=files, data=fields)
+        return res.text
+
+
+def captcha_recognizer():
+    y = Connection(
+        username='wslshanlin',
+        password='1994180438',
+        appid=4149,
+        appkey='8576e00849d04f656c2bec09d0f01018',
+    )
+    y.login()
+    return y
+
+
+recognizer = captcha_recognizer()
+
+if __name__ == '__main__':
+    username = 'wslshanlin'
+    password = '1994180438'
+    appid = 4149
+    appkey = '8576e00849d04f656c2bec09d0f01018'
+    filename = 'getimage.jpg'
+    codetype = 4006
+    timeout = 60
+
+    # 检查
+    if (username == 'username'):
+        print('请设置好相关参数再测试')
+    else:
+        # 初始化
+        y = Connection(username, password, appid, appkey)
+        # 登陆云打码
+        uid = y.login()
+        print('uid: %s' % uid)
+        # 查询余额
+        balance = y.balance()
+        print('balance: %s' % balance)
+        # 开始识别，图片路径，验证码类型ID，超时时间（秒），识别结果
+        cid, result = y.decode(filename, codetype, timeout)
+        print('cid: %s, result: %s' % (cid, result))
