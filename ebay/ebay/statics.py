02bf0cf0804c09e572a35eec748d6b2e036322a4
hanlin <hanlin>
2017-11-13 14:28:09 +0800

    newFeature |

--- ebay/ebay/statics.py
+++ ebay/ebay/statics.py
@@ -98,6 +98,7 @@ class Cleaner():
         data['quantitySoldLastWeek'], data['quantitySoldTwoWeeksAgo'] = self.sales_last_week(item)
         data['quantitySoldYesterday'] = self.sales_yesterday(item)
         data['topCategoryID'] = self.category_id_top(item)
+        self.redis.hset('ebay:log', item['itemId'], data)
         return data
 
     def add_up_shop(self, item_cleaned):
